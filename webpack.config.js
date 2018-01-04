476256989ac534892f60be35222ab29fc5736cb2
1forh <1forhlol@gmail.com>
2018-01-04 09:15:27 -0500

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,74 @@
-// dependencies
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-// configure rules for scripts
-const scripts = {
-	test: /\.(js)$/,
-	loader: 'babel-loader',
-	options: {
-		presets: ['env']
-	}
+const DEFAULT_PORT = 3000;
+
+module.exports = function (env = { production: false }) {
+	const isProduction = env.production === true;
+	console.info('Environment: ', env);
+
+	const styleLoaders = [
+		{
+			test: /\.scss$/,
+			use: ExtractTextPlugin.extract({
+				fallback: 'style-loader',
+				use: [
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							parser: 'postcss-scss',
+							plugins: [
+								require('autoprefixer')(),
+								isProduction ? require('cssnano')() : () => {}
+							]
+						}
+					},
+					'sass-loader'
+				]
+			})
+		}
+	];
+
+	const scriptLoaders = [
+		{
+			test: /\.(js)$/,
+			loader: 'babel-loader',
+			options: {
+				presets: ['env']
+			}
+		}
+	];
+
+	const config = {
+		entry: {
+			'bundle.css': path.resolve(__dirname, './test/styles/main.scss'),
+			'bundle.js': path.resolve(__dirname, './test/scripts/main.js')
+		},
+		devtool: 'source-map',
+		module: {
+			loaders: [...styleLoaders, ...scriptLoaders]
+		},
+		plugins: [
+			isProduction ? new UglifyJSPlugin() : () => {},
+			new ExtractTextPlugin('bundle.css'),
+			new CleanWebpackPlugin(path.resolve(__dirname, './test/dist')),
+			new BrowserSyncPlugin({
+				port: process.env.PORT || DEFAULT_PORT
+			}),
+			new webpack.DefinePlugin({
+				'process.env.NODE_ENV': isProduction ? JSON.stringify('production') : JSON.stringify('development')
+			})
+		],
+		output: {
+			filename: '[name]',
+			path: path.resolve(__dirname, './test/dist')
+		}
+	};
+
+	return config;
 };
-
-// configure webpack
-const config = {
-	entry: {
-		main: [
-			path.resolve(__dirname, './test/scripts/main.js')
-		]
-	},
-	devtool: 'source-map',
-	devServer: {
-		contentBase: path.resolve(__dirname, './test')
-	},
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, './test/dist/scripts')
-	},
-	module: {
-		rules: [scripts]
-	}
-};
-
-// export the config object
-module.exports = config;
